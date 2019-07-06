25944c743279227e00fb6e02281b3d394b87f941
unknown <nemanja.ranisavljevic.9977@gmail.com>
2019-07-06 16:17:28 +0200

    prvi-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+import { RootComponentComponent } from './root/root-component/root-component.component';
+import { HttpClientModule } from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+// import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { Routes, RouterModule } from '@angular/router';
+// import { RootModule } from './root/root.module';
+// import { HomeModule } from './home/home.module';
+
+const routes : Routes = [
+  {
+    path : '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: RootComponentComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: './home/home.module#HomeModule'  
+      }
+    ]
+  }
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RootComponentComponent,
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
-    AppRoutingModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/root/root.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RootRoutingModule } from './root-routing.module';
+import { RootComponentComponent } from './root-component/root-component.component';
+
+@NgModule({
+  declarations: [RootComponentComponent],
+  imports: [
+    CommonModule,
+    RootRoutingModule
+  ]
+})
+export class RootModule { }
